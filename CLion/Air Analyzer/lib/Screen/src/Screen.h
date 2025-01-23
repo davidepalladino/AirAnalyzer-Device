2c6c04691db2b41cb355dff48f9725258e5d4f8a
davidepalladino <davidepalladino@hotmail.com>
2025-01-23 19:00:45 +0100

    Refactored `Screen`

--- CLion/Air Analyzer/lib/Screen/src/Screen.h
+++ CLion/Air Analyzer/lib/Screen/src/Screen.h
@@ -21,8 +21,8 @@
 #include <U8g2lib.h>
 #include <AbstractObserver.h>
 #include <Sensor.h>
-#include "configScreen.h"
-#include "iconsScreen.h"
+#include "config.h"
+#include "icons.h"
 
 class Sensor;
 
@@ -35,64 +35,64 @@ class Sensor;
 class Screen : private AbstractObserver {
     friend class AbstractSubject;
 
-public:
-    /**
-     * @brief Constructs a Screen object.
-     * @param sensor Reference to the Sensor object.
-     * @param pinSCL SCL pin connected to the screen.
-     * @param pinSDA SDA pin connected to the screen.
-     */
-    Screen(Sensor &sensor, uint8_t pinSCL, uint8_t pinSDA);
+    public:
+        /**
+         * @brief Constructs a Screen object.
+         * @param sensor Reference to the Sensor object.
+         * @param pinSCL SCL pin connected to the screen.
+         * @param pinSDA SDA pin connected to the screen.
+         */
+        Screen(Sensor &sensor, uint8_t pinSCL, uint8_t pinSDA);
 
-    /** @brief Initializes the screen. */
-    void begin();
+        /** @brief Initializes the screen. */
+        void begin();
 
-    /** @brief Sets the room ID. */
-    void setRoomNumber(uint8_t roomNumber);
+        /** @brief Sets the room ID. */
+        void setRoomNumber(uint8_t roomNumber);
 
-    /** @brief Sets and gets the WiFi connection status. */
-    void setIsConnected(bool isConnected);
-    bool getIsConnected();
+        /** @brief Sets and gets the WiFi connection status. */
+        void setIsConnected(bool isConnected);
+        bool getIsConnected();
 
-    /** @brief Sets and gets the update status. */
-    void setIsUpdated(bool isUpdated);
-    bool getIsUpdated();
+        /** @brief Sets and gets the update status. */
+        void setIsUpdated(bool isUpdated);
+        bool getIsUpdated();
 
-    /** @brief Sets and gets the screen visibility status. */
-    void setIsViewable(bool isViewable);
-    bool getIsViewable();
+        /** @brief Sets and gets the screen visibility status. */
+        void setIsViewable(bool isViewable);
+        bool getIsViewable();
 
-    // INSTALLATION AND CONFIGURATION VIEWS
-    void showInstallationRoomIDPage(const String messages[3]);
-    void showInstallationWiFiPage(const String messages[6], uint8_t result);
-    void showUpgradeVersionThreePage(const String *messages, const String &localIP);
+        // INSTALLATION AND CONFIGURATION VIEWS
+        void showInstallationRoomIDPage(const String messages[3]);
+        void showInstallationWiFiPage(const String messages[6], uint8_t result);
+        void showUpgradeVersionThreePage(const String *messages, const String &localIP);
 
-    // ORDINARY VIEWS
-    void showBrand(const String &version);
-    void showLoadingPage(const String &message, float percentage);
-    void showMainPage();
-    void clear();
-    void showMessagePage(const String &message);
-    void showMessagePage(const String messages[2]);
+        // ORDINARY VIEWS
+        void showBrand(const String &version);
+        void showLoadingPage(const String &message, float percentage);
+        void showMainPage();
+        void clear();
+        void showMessagePage(const String &message);
+        void showMessagePage(const String messages[2]);
 
-private:
-    U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C *screen;
-    Sensor &sensor;
-    uint8_t roomNumber;
-    bool isConnected;
-    bool isUpdated;
-    bool isViewable;
+    private:
+        U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C *screen;
+        Sensor &sensor;
+        uint8_t roomNumber;
+        bool isConnected;
+        bool isUpdated;
+        bool isViewable;
 
-    void drawBrand();
-    void drawVersion(const String &version);
-    void drawBar(float percentage);
-    void drawMessage(uint8_t positionX, uint8_t positionY, const String &message);
-    void drawRoomID();
-    void drawTemperature();
-    void drawHumidity();
-    void drawWiFiStatus();
-    void drawUpdateStatus();
-    void update();
+        void drawBrand();
+        void drawVersion(const String &version);
+        void drawBar(float percentage);
+        void drawMessage(uint8_t positionX, uint8_t positionY, const String &message);
+        void drawRoomID();
+        void drawTemperature();
+        void drawHumidity();
+        void drawWiFiStatus();
+        void drawUpdateStatus();
+        void update();
 };
 
 #endif // SCREEN_H
